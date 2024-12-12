ae9ba07d0b48461985d0fad8ad1eeca60902fbb7
sonu79934 <yogasritha8@gmail.com>
2024-12-12 13:50:28 +0530

    Add files via upload

---
+++ longint.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class longint{
+    public static void main(String args[]){
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextInt();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
---
+++ longintwithlongmethod.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class longintwithlongmethod{
+    public static void main(String args[]){
+        long ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
